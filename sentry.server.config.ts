45f9ae22c523840e15af09dd7e26b086815bbb61
ArunKushhhh <austen.dezigns.dev@gmail.com>
2026-03-01 05:44:45 +0530

    feat: sentry setup
    Signed-off-by: ArunKushhhh <austen.dezigns.dev@gmail.com>

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://163dced697758085021b47cb569f3677@o4510966191423488.ingest.us.sentry.io/4510966301523968",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
